fed200595abb861b51f7a613dea21bea98d27040
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2022-04-10 00:54:39 +0800

    Update main

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from Detector import *
 modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
 
 classfile = "coco.names"
-imagepath = 'test/3.jpg'
-# videopath = 'test/8.mp4'
+# imagepath = 'test/6.jpg'
+videopath = 'test/9.mp4'
 # videopath = 0 # for webcam
 threshold = 0.5
 
@@ -12,5 +12,5 @@ detector = Detector()
 detector.readClasses(classfile)
 detector.downloadModel(modelURL)
 detector.loadModel()
-detector.predictImage(imagepath, threshold)
-# detector.predictVideo(videopath, threshold)
+# detector.predictImage(imagepath, threshold)
+detector.predictVideo(videopath, threshold)
